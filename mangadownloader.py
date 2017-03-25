6cd6abc8259f4870bc0924eec721bb4d052939d0
Eren <eren.kibar97@gmail.com>
2017-03-25 21:51:38 +0200

    Initial commit

--- mangadownloader.py
+++ mangadownloader.py
@@ -1,5 +1,5 @@
 #    A simple command-line tool to download manga.
-#    Copyright (C) <year>  <name of author>
+#    Copyright (C) 2017  Eren Kibar
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
